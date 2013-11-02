6e57c05d009b5f01842018efc26c460edf81af28
Paul Pilone <paul@element84.com>
2013-11-01 22:13:55 -0400

    Initial commits with kiwi snippets.

---
+++ kiwiexample.m
@@ -0,0 +1,10 @@
+// Kiwi Example
+// Simple Kiwi example outline.
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
+it(<#example name#>, ^{
+  <#expectation#>        
+});
---
+++ kiwispec.m
@@ -0,0 +1,22 @@
+// Kiwi Spec
+// Simple outline for a default Kiwi Spec with example group.
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(<#spec name#>)
+
+describe(<#class name#>, ^{
+    context(<#content name#>, ^{
+
+      it(<#example name#>, ^{
+
+      });
+
+    });
+});
+
+SPEC_END
